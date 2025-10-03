ec5bcffd43ea0afb389c7b8c1862cc1b7704c256
lordjson8 <lordjson8@gmail.com>
2025-10-03 07:01:12 +0100

    first commit

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -3,6 +3,7 @@ import Animated from 'react-native-reanimated';
 export function HelloWave() {
   return (
     <Animated.Text
+    className={'bg-red-500'}
       style={{
         fontSize: 28,
         lineHeight: 32,
